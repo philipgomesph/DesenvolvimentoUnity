ddc0110574b439112854ea7a81683918652a6ee3
Philipe PH <philip.gomesph@gmail.com>
2021-11-16 09:50:03 -0300

    Aula 6 - Fix boas praticas unity

--- Aulas - Dechichi/Projeto 1/Primeiro Projeto/Assets/PlayerController.cs
+++ Aulas - Dechichi/Projeto 1/Primeiro Projeto/Assets/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public Rigidbody rigidbody;  // publico/ tipo do componente / nome
-
+    public int Forca_X, Forca_Z=50;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,19 +15,22 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()  //Boas praticas Unity == Tudo q for trabalhar com fisica, é bom usar FixedUpdate !
     {
 
 //MOVIMENTO DE PERSONAGEM
+    rigidbody.AddForce(0,0,Forca_Z);
      if( Input.GetKey("a") ==true) //Se a tecla "a" esta apertada ou seja == a true
      {
-         rigidbody.AddForce(-2,0,0); // anda -2 em X
+         rigidbody.AddForce(-Forca_X*Time.fixedDeltaTime,0,0); // anda -2 em X  //usando variaveis em vez de numero interios  (boas praticas)
      }
      if(Input.GetKey("d") == true)//Se a tecla "d" esta apertada ou seja == a true 
      {
-         rigidbody.AddForce(2,0,0); // anda 2 em X
+         rigidbody.AddForce(Forca_X*Time.fixedDeltaTime,0,0); // anda 2 em X
+         //time.fixedDeltaTime == traz pra realidade(n faz a soma da variaveis toda vez por frame)
+         // ou seja-> X*0.016 em vez de X*FPS
      }
-     if(Input.GetKey("x") ==true) // Lancamento 
+     if(Input.GetKey("x") ==true) // Lancamento   // N esta usando boas praticas Unity
      {
          rigidbody.AddForce(0,50,200);
      }   
